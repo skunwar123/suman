135f1d181d5a2f34dce1fa863c03d567004ef9aa
skunwar123 <sumankunwar152@gmail.com>
2022-07-06 12:01:46 +0545

    sumantest3

--- sample/Program.cs
+++ sample/Program.cs
@@ -11,7 +11,9 @@ namespace MyApp
             {
             Car myobj = new Car();
             Console.WriteLine(myobj.color);
+            Console.WriteLine(myobj.color);
             Console.WriteLine(myobj.maxSpeed );
+            Console.WriteLine(myobj.maxSpeed);
             Console.WriteLine(myobj.maxSpeed);
         }
 
