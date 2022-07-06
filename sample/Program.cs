1c32d6175845134f268e51c5aaba12fbeac7ea78
skunwar123 <sumankunwar152@gmail.com>
2022-07-06 11:43:33 +0545

    sumantest

---
+++ sample/Program.cs
@@ -0,0 +1,20 @@
+﻿
+ using System;
+
+namespace MyApp
+{
+    class Car
+    {
+        string color = "red";
+        int maxSpeed = 200;
+        static void Main(string[] args)
+            {
+            Car myobj = new Car();
+            Console.WriteLine(myobj.color);
+            Console.WriteLine(myobj.maxSpeed );
+                }
+
+    }
+}
+
+    
